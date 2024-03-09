70ed15bc4f0ef0d0560be094ec1065413eaa0aa4
madhav-sureify <madhav.gedela@sureify.com>
2024-03-09 20:58:13 +0530

    modified stories

--- packages/ui-components/src/components/textFieldController/TextFieldController.stories.tsx
+++ packages/ui-components/src/components/textFieldController/TextFieldController.stories.tsx
@@ -1,5 +1,9 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { TextFieldController } from "./TextFieldController";
+import {
+  TextFieldController,
+  TextFieldControllerConfig,
+} from "./TextFieldController";
+import { useForm } from "react-hook-form";
 
 const meta = {
   title: "Components/TextFieldController",
@@ -11,5 +15,13 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
-  args: { name: "password", controllerProps: {}, textFieldProps: {} },
+  args: {
+    name: "password",
+    textFieldProps: {},
+    controllerProps: {},
+  },
+  render: (args: TextFieldControllerConfig) => {
+    const { control } = useForm();
+    return <TextFieldController {...args} controllerProps={{ control }} />;
+  },
 };
