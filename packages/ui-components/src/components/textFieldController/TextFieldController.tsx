aa6dbdc3c3df8c5e69acbe830eb5305b02849b82
madhav-sureify <madhav.gedela@sureify.com>
2024-03-09 20:26:57 +0530

    added new component in ui-components-textFieldController

---
+++ packages/ui-components/src/components/textFieldController/TextFieldController.tsx
@@ -0,0 +1,37 @@
+import { TextField, TextFieldProps } from "@mui/material";
+import {
+  Controller,
+  ControllerProps,
+  FieldValues,
+  Path,
+} from "react-hook-form";
+
+export interface TextFieldControllerConfig<
+  Inputs extends FieldValues = FieldValues
+> {
+  readonly name: Path<Inputs>;
+  readonly textFieldProps: TextFieldProps;
+  readonly controllerProps: Omit<ControllerProps<Inputs>, "render" | "name">;
+}
+
+export const TextFieldController = <Inputs extends FieldValues = FieldValues>({
+  name,
+  textFieldProps,
+  controllerProps,
+}: TextFieldControllerConfig<Inputs>) => {
+  return (
+    <>
+      <Controller
+        name={name}
+        render={({ field, fieldState: { error } }) => (
+          <TextField
+            {...textFieldProps}
+            {...field}
+            helperText={textFieldProps.helperText || error?.message}
+          />
+        )}
+        {...controllerProps}
+      />
+    </>
+  );
+};
